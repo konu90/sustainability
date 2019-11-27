8ca10dc2cc819b6df6b9c494c1264506ccb1bb01
Axoahi <jmba12@gcloud.ua.es>
2019-11-27 13:28:14 +0100

    Solucionado problema con Regex

--- webscrapping.py
+++ webscrapping.py
@@ -14,6 +14,7 @@
 #     name: python3
 # ---
 
+import itertools
 import pandas as pd
 import re
 import numpy as np
@@ -23,32 +24,32 @@ def extraeWeb (webscrap):
     auxInput = []
 
     # Obtén fecha
-    regex = "dt=.\d*\"\>(\d\d\/\d\d\/\d\d)"
+    regex = "dt=.\d*\"\>(\d\d\/\d\d\/\d\d|-)"
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
     # Obtén energía generada (Generated)
-    regex = "title=\"Exported: None\">(\d*\.\d*)"
+    regex = "title=\"Exported: None\">(\d*\.\d*|-)"
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
     # Obtén eficiencia (efficiency)
-    regex = "style=\"padding-right:25px\">(.d*\.\d*)"
+    regex = "style=\"padding-right:25px\">(.d*\.\d*|-)"
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
     # Obtén pico de potencia (peak power)
-    regex = "style=\"padding-right:35px\">(\d*\.\d*)"
+    regex = "style=\"padding-right:35px\">(\d*\.\d*|-)"
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
     # Obtén hora de pico (peak time)
-    regex = "<td align=\"center\">(\d*\:\d*..)"
+    regex = "<td align=\"center\">(\d*\:\d*..|-)"
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
     # Obtén tipo de día (condictions)
-    regex = "<td nowrap=\"\">(\w*\s\w*|\w*)</td><td align=\"right\""
+    regex = "<td nowrap=\"\">(\w*\s\w*|\w*|-)</td><td align=\"right\""
     matches = re.findall(regex, webscrap, re.MULTILINE)
     auxInput.append(matches)
 
